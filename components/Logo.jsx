cf08e5a304b3eb4c144d99598539b1248abf0b28
nid_ch <ne.chenni@gmail.com>
2022-04-14 17:12:54 +0100

    last commit

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function Logo() {
     return (
         <div className="flex items-center justify-center w-full h-[calc(25vh)]">
-           <Image src="/images/magnusAll.png" width={800} height={120}/>
+           <Image src="/images/magnusAll.png" width={900} height={120}/>
         </div>
     )
 }
